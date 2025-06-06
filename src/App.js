9142099093fbf9d1e19fe1267e3089bb76adaaf3
Vedant2314 <vedantwairkar@gmail.com>
2025-06-06 15:22:53 +0530

    Initial commit with welcome message

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          <h1>Welcome to my Github</h1>
         </p>
         <a
           className="App-link"
