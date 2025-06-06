071d0509292fd9ce7642a78e80dacca9988ab13c
Vedant2314 <vedantwairkar@gmail.com>
2025-06-06 15:33:59 +0530

    Added ordered list in App.js

--- src/App.js
+++ src/App.js
@@ -3,22 +3,12 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-          <h1>Welcome to my Github</h1>
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Welcome to Github Message</h2>
+      <ol>
+        <li>Learn Commands</li>
+        <li>Verify the results</li>
+      </ol>
     </div>
   );
 }
